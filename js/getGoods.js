c999e54223375e3e334358297ad4c83f296c30f3
Popov Andrey <71716659+AnDrEyScode@users.noreply.github.com>
2022-02-26 01:19:30 +0300

    Add files via upload

---
+++ js/getGoods.js
@@ -0,0 +1,33 @@
+let getGoods = () => {
+
+  const getData = () => {
+    fetch('/db/db.json')
+    .then((response) => response.json())
+    .then((data) => {
+      localStorage.setItem('goods', JSON.stringify(data))
+    })
+  }
+
+  const links = document.querySelectorAll(".navigation-link")
+
+  links.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault()
+      getData()
+    })
+  })
+
+  // localStorage.setItem('goods', JSON.stringify({name: 'all'}))
+
+  // const goods = JSON.parse(localStorage.getItem('goods'))
+
+  // console.log(localStorage)
+
+  // localStorage.removeItem('goods')
+
+  // console.log(localStorage)
+
+
+}
+
+getGoods()
